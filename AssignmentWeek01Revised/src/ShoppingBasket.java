2130fc2a3bb085fed83695df85b86e5340697000
Prabudh <prabudh.gautam@ria.insure>
2023-08-14 09:06:09 +0530

    added unit tests

--- AssignmentWeek01Revised/src/ShoppingBasket.java
+++ AssignmentWeek01Revised/src/ShoppingBasket.java
@@ -67,8 +67,9 @@ public class ShoppingBasket {
 //        if(!type.equals("book") && !type.equals("food") && !type.equals("medical product")) sales_tax += (float) (rate*0.1);
 
         sales_tax = Math.round( sales_tax * 20.0) / (float)20.0;
+        sales_tax = Float.parseFloat(String.format("%.2f", sales_tax));
+//        sales_tax *= quantity;
         good.setRate(rate+sales_tax);
-
         good.setSales_tax(sales_tax*quantity);
     }
 
